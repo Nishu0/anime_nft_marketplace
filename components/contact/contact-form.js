cfee1ae37511286075eb2901b41358705f47875f
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-08-01 07:18:40 +0000

    Deploy error

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -3,7 +3,6 @@
 
 import { useState, useEffect } from 'react';
 
-import classes from './contact-form.module.css';
 import Notification from '../ui/notification';
 
 async function sendContactData(contactDetails) {
