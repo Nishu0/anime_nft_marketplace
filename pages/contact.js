6d0162aaea51242829826c998b1a306596076b27
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-08-01 07:14:22 +0000

    Contact Page Ready for Production

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import ContactForm from "../components/contact/contact-form";
+export const metadata = {
+    title: 'Contact Me',
+    description: 'Send me your messages!',
+};
+function ContactPage() {
+    return (
+        <ContactForm />
+    )
+}
+
+export default ContactPage;
